929199c79c6c67e785e50047ae94193273d3e2e0
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2018-07-09 23:31:42 +0800

    hot fix 2

--- functions.py
+++ functions.py
@@ -10,17 +10,17 @@ def tap_screen(x, y,device_x,device_y):
     os.system('adb shell input tap {} {}'.format(real_x, real_y))
 
 def first_working(wait,device_x,device_y):
-    logging.debug('# First time...')
+    logging.debug('#1 First time...')
     tap_screen(1450, 910,device_x,device_y)
     time.sleep(wait[0])
 
-    logging.debug('# Auto mode active...')
+    logging.debug('#2 Auto mode active...')
     tap_screen(1780, 40,device_x,device_y)        
     for i in range(wait[1]):
         tap_screen(1720, 80,device_x,device_y)
         time.sleep(1)
 
-    logging.debug('# Repeating...\n')
+    logging.debug('#3 Repeating...\n')
     tap_screen(1600, 980,device_x,device_y)
     time.sleep(wait[2])
 
@@ -54,14 +54,14 @@ def main(wait,device_x,device_y):
     #判断模块
     if gain_money >= 0 and gain_money <= 4200:
         repeat = 1 + int(gain_money / 19)
-        print('还有' + str(repeat) + '次完成')
+        print('还有' + str(repeat) + '次完成\n')
         
         print('三秒后开始')
         print('3')
         time.sleep(1)
         print('2')
         time.sleep(1)        
-        print('1')
+        print('1\n')
         time.sleep(1)
 
         #初始化
