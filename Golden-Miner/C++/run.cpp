5617356ff8a8f332368be4651e82bd5d4819cec4
铭轩 梁 <807082193@qq.com>
2019-03-06 12:57:51 +0800

    增加端口号自定义，优化计时，原来的配置文件需要删除重新生成

--- Golden-Miner/C++/run.cpp
+++ Golden-Miner/C++/run.cpp
@@ -4,11 +4,14 @@ int main()
 	//屏幕分辨率
 	int device_x = 1440, device_y = 810;
 
+	//端口号
+	int port = 7555;
+
 	// 各步骤等待间隔
 	//step_wait[0]为加载时间，不同手机加载速度不同
-	//step_wait[1]为战斗时间
-	//step_wait[2]为结算时间	
-	int wait[3] = { 12,30,3 };
+	//step_wait[1]为战斗+结算时间
+	//step_wait[2]为重开等待时间
+	int wait[3] = { 12,35,3 };
 
 	FILE *fp = fopen("Settings.txt", "r+");
 	if (fp == NULL)
@@ -16,7 +19,7 @@ int main()
 		printf("未检测到设置文件!正在生成...\n");
 
 		fp = fopen("Settings.txt", "w+");
-		fprintf(fp, "分辨率 = %d X %d, 等待时间 = { %d,%d,%d }", device_x, device_y, wait[0], wait[1], wait[2]);
+		fprintf(fp, "分辨率 = %d X %d, 等待时间 = { %d,%d }, 端口号 = %d", device_x, device_y, wait[0], wait[1], port);
 		fclose(fp);
 
 		printf("生成完毕!程序退出!\n");
@@ -26,7 +29,7 @@ int main()
 	else
 	{
 		//读取文件
-		int temp = fscanf(fp, "分辨率 = %d X %d, 等待时间 = { %d,%d,%d }", &device_x, &device_y, &wait[0], &wait[1], &wait[2]);
+		int temp = fscanf(fp, "分辨率 = %d X %d, 等待时间 = { %d,%d }, 端口号 = %d", &device_x, &device_y, &wait[0], &wait[1], &port);
 		//检查读取的变量个数
 		if (temp != 5)
 		{
@@ -41,8 +44,8 @@ int main()
 	fclose(fp);
 
 	cout << "当前分辨率为 " << device_x << " X " << device_y << endl;
-	cout << "等待时间为 " << wait[0] << "," << wait[1] << "," << wait[2] << endl;
-	cout <<endl<< "注意!第一次使用请确保进入了自动战斗模式!" << endl;
+	cout << "等待时间为 " << wait[0] << "," << wait[1] << endl;
+	cout << endl << "注意!第一次使用请确保进入了自动战斗模式!" << endl;
 	system("pause");
 
 	//主程序 
@@ -56,7 +59,7 @@ int main()
 	Sleep(1000);
 
 	//虚拟机adb初始化
-	adb_init();
+	adb_init(port);
 
 	//循环模块 
 	for (int i = 1; i < 223; i++)
@@ -67,10 +70,15 @@ int main()
 		Sleep(wait[0] * 1000);
 
 		cout << "#2 Waiting..." << endl;
-		for (int j = 0; j < 2*wait[1]; j++)
+		for (int j = 0; j < wait[1]; j++)
 		{
-			tap_screen(real(1720, 80, device_x, device_y));
-			Sleep(200);
+			DWORD start = GetTickCount(); //开始计时
+			{
+				tap_screen(real(1720, 80, device_x, device_y));
+			}
+			DWORD end = GetTickCount(); //结束计时
+			int time = 1000 - (end - start);
+			Sleep(time);
 		}
 
 		cout << "#3 Repeating..." << endl;
